bfadd359052f42e9c04244bee011a16ef3d935cc
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-09 00:37:31 +0530

    Add project files.

---
+++ Findout largest element.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_the_digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = { 1, 222, 3, 5, 7, 80, 9, 10,11 };
+            int temp = 0;
+
+
+            for (int i = 0; i < ar.Length; i++)
+            
+               // for (int J = i + 1; J < ar.Length; i++)
+                
+                    if (ar[i] > temp)
+                    {
+                        temp = ar[i];
+                    }
+                    Console.WriteLine("Largest element is = " + temp);
+                }
+            }
+
+        }
+    
+
+
+    
+
+
